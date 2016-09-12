e2c9414061d9d81286ba43da4c9426a72dd6993c
Theo Markettos <atm26@cl.cam.ac.uk>
2016-09-12 16:38:54 +0100

    Add an ECALL handler to catch the end of program in hardware

--- software/src/init.s
+++ software/src/init.s
@@ -5,7 +5,13 @@
 entry:
 
     la    sp, __sp-32   # set up the stack pointer, using a constant defined in the linker script.
+
+    la    t0, end       # on hardware, ECALL doesn't stop the CPU, so define
+                        # a handler to catch the ECALL and spin
+    csrrw zero,0x305,t0 # set the address of the handler (CSR 0x305 is the trap handler base register)
+
     call  main          # call the main function
     ecall               # halt the simluation when it returns
+
 end:
-	j end               # loop when finished, since there is no environment to return to.
+    j end               # loop when finished if there is no environment to return to.
